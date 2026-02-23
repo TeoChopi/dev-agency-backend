512098bb9fc6194ea0fa9847db2be9e8654074ae
Teofilo Gonzalez Sanchez <56451681+TeoChopi@users.noreply.github.com>
2026-02-23 16:59:59 +0100

    feat(TASK-001): app/api/v1/api.py

---
+++ app/api/v1/api.py
@@ -0,0 +1,11 @@
+"""API v1 router aggregation"""
+from fastapi import APIRouter
+
+from app.api.v1.endpoints import products
+
+api_router = APIRouter()
+api_router.include_router(
+    products.router, 
+    prefix="/products", 
+    tags=["products"]
+)
