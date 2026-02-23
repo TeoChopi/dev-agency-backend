79abdfa51f571aff96754fe965d2b92c2971a618
Teofilo Gonzalez Sanchez <56451681+TeoChopi@users.noreply.github.com>
2026-02-23 21:49:03 +0100

    feat(TASK-001): src/utils/logger.ts

---
+++ src/utils/logger.ts
@@ -0,0 +1,22 @@
+import pino from 'pino';
+import { config } from '@/config';
+
+export const logger = pino({
+  level: config.logging.level,
+  transport: config.app.isDevelopment
+    ? {
+        target: 'pino-pretty',
+        options: {
+          colorize: true,
+          ignore: 'pid,hostname',
+          translateTime: 'yyyy-mm-dd HH:MM:ss',
+        },
+      }
+    : undefined,
+  formatters: {
+    level: (label) => {
+      return { level: label };
+    },
+  },
+  timestamp: () => `,"timestamp":"${new Date().toISOString()}"`,
+});
